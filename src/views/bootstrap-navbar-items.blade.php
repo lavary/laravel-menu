c741bf414ae98ea7ffd440abd08613e8ddfbd105
Reza Lavaryan <mrl.8081@gmail.com>
2014-08-04 00:22:31 +0430

    Built-in Bootstrap friendly item renderer

---
+++ src/views/bootstrap-navbar-items.blade.php
@@ -0,0 +1,22 @@
+@foreach($items as $item)
+  <li@lm-attrs($item) @if($item->hasChildren()) class =      "   dropdown good yes " data-model = " nice-one     " @endif @lm-endattrs>
+      @if($item->link)<a{{$item->builder->attributes($item->link->attr())}} href="{{ $item->url() }}" @if($item->hasChildren()) class="dropdown-toggle" data-toggle="dropdown" @endif>
+        {{ $item->title }}
+        @if($item->hasChildren()) <b class="caret"></b> @endif
+      </a>
+      @else
+        {{$item->title}}
+      @endif
+      @if($item->hasChildren())
+        <ul class="dropdown-menu">
+              @include('laravel-menu::bootstrap-navbar-items', 
+                array('items' => $item->children()))
+        </ul> 
+      @endif
+  </li>
+  @if($item->divider)
+  	<li{{$item->builder->attributes($item->divider)}}></li>
+  @endif
+@endforeach
+
+<br>
