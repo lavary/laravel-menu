ba083d30b4f84a2a1d00a459add48962601de94d
Ben Swinburne <ben.swinburne@gmail.com>
2015-03-19 15:59:39 +0000

    Fixed mad spacing and a syntax error

--- src/Lavary/Menu/ServiceProvider.php
+++ src/Lavary/Menu/ServiceProvider.php
@@ -5,74 +5,74 @@ use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 class ServiceProvider extends BaseServiceProvider
 {
-    /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = false;
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
 
-    /**
-     * Provider for version
-     *
-     * @var \Illuminate\Support\ServiceProvider
-     */
-    protected $provider;
+	/**
+	 * Provider for version
+	 *
+	 * @var \Illuminate\Support\ServiceProvider
+	 */
+	protected $provider;
 
-    /**
-     * Create a new service provider instance.
-     *
-     * @param  \Illuminate\Contracts\Foundation\Application  $app
-     * @return void
-     */
-    public function __construct(Application $app)
-    {
-        parent::__construct($app);
+	/**
+	 * Create a new service provider instance.
+	 *
+	 * @param  \Illuminate\Contracts\Foundation\Application  $app
+	 * @return void
+	 */
+	public function __construct(Application $app)
+	{
+		parent::__construct($app);
 
-        $this->provider = $this->getProvider();
-    }
+		$this->provider = $this->getProvider();
+	}
 
-    /**
-     * Bootstrap the application events.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        return $this->provider->boot();
-    }
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		return $this->provider->boot();
+	}
 
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        return $this->provider->register();
-    }
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		return $this->provider->register();
+	}
 
-    /**
-     * Return ServiceProvider suitable for Laravel version
-     *
-     * @return \Lavary\Menu\Providers\ProviderInterface
-     */
-    private function getProvider()
-    {
-        $provider = version_compare(Application::VERSION, '5.0', '<'))
-            ? '\Lavary\Menu\Providers\Laravel4'
-            : '\Lavary\Menu\Providers\Laravel5';
+	/**
+	 * Return ServiceProvider suitable for Laravel version
+	 *
+	 * @return \Lavary\Menu\Providers\ProviderInterface
+	 */
+	private function getProvider()
+	{
+		$provider = version_compare(Application::VERSION, '5.0', '<')
+			? '\Lavary\Menu\Providers\Laravel4'
+			: '\Lavary\Menu\Providers\Laravel5';
 
-        return new $provider($this->app);
-    }
+		return new $provider($this->app);
+	}
 
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        return array('menu');
-    }
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array('menu');
+	}
 }
