ac33baea6f7659efa6ad8da23879be04615727f6
Chris Watts <Chris@CJWatts.com>
2018-02-27 13:36:29 +0000

    Make bootstrap view compatible with Bootstrap 4
    Added 'nav-link' to the <a> class.

--- src/Lavary/Menu/resources/views/bootstrap-navbar-items.blade.php
+++ src/Lavary/Menu/resources/views/bootstrap-navbar-items.blade.php
@@ -1,6 +1,6 @@
 @foreach($items as $item)
-  <li@lm-attrs($item) @if($item->hasChildren())class ="dropdown"@endif @lm-endattrs>
-    @if($item->link) <a@lm-attrs($item->link) @if($item->hasChildren()) class="dropdown-toggle" data-toggle="dropdown" @endif @lm-endattrs href="{!! $item->url() !!}">
+  <li@lm-attrs($item) @if($item->hasChildren()) class="dropdown" @endif @lm-endattrs>
+    @if($item->link) <a@lm-attrs($item->link) @if($item->hasChildren()) class="nav-link dropdown-toggle" data-toggle="dropdown" @else class="nav-link" @endif @lm-endattrs href="{!! $item->url() !!}">
       {!! $item->title !!}
       @if($item->hasChildren()) <b class="caret"></b> @endif
     </a>
