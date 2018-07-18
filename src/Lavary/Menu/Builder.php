lavary/laravel-menu

commit c9be1d2e724077576cde4c394c1483d121723457

Clean up all docblock return type hints. Convert arrays to short arrays.

src/Lavary/Menu/Builder.php
@@ -30,14 +30,14 @@ class Builder
      *
      * @var array
      */
-    protected $groupStack = array();
+    protected $groupStack = [];
 
     /**
      * The reserved attributes.
      *
      * @var array
      */
-    protected $reserved = array('route', 'action', 'url', 'prefix', 'parent', 'secure', 'raw');
+    protected $reserved = ['route', 'action', 'url', 'prefix', 'parent', 'secure', 'raw'];
 
     /**
      * Initializing the menu manager.
@@ -90,7 +90,7 @@ class Builder
      *
      * @return Item
      */
-    public function raw($title, array $options = array())
+    public function raw($title, array $options = [])
     {
         $options['raw'] = true;
 
@@ -104,9 +104,7 @@ class Builder
      */
     public function get($title)
     {
-        return $this->whereNickname($title)
-
-                    ->first();
+        return $this->whereNickname($title)->first();
     }
 
     /**
@@ -116,9 +114,7 @@ class Builder
      */
     public function find($id)
     {
-        return $this->whereId($id)
-
-                    ->first();
+        return $this->whereId($id)->first();
     }
 
     /**
@@ -160,9 +156,7 @@ class Builder
      */
     public function item($title)
     {
-        return $this->whereNickname($title)
-
-                    ->first();
+        return $this->whereNickname($title)->first();
     }
 
     /**
@@ -180,7 +174,7 @@ class Builder
      *
      * @param array $attributes
      */
-    public function divide(array $attributes = array())
+    public function divide(array $attributes = [])
     {
         $attributes['class'] = self::formatGroupClass(array('class' => 'divider'), $attributes);
 
@@ -210,7 +204,7 @@ class Builder
      *
      * @param array $attributes
      */
-    protected function updateGroupStack(array $attributes = array())
+    protected function updateGroupStack(array $attributes = [])
     {
         if (count($this->groupStack) > 0) {
             $attributes = $this->mergeWithLastGroup($attributes);
@@ -317,10 +311,10 @@ class Builder
      *
      * @return array
      */
-    public function extractAttributes($options = array())
+    public function extractAttributes($options = [])
     {
         if (!is_array($options)) {
-            $options = array();
+            $options = [];
         }
 
         if (count($this->groupStack) > 0) {
@@ -386,7 +380,7 @@ class Builder
             return $url;
         }
 
-        return \URL::to($prefix.'/'.$url, array(), $secure);
+        return \URL::to($prefix.'/'.$url, [], $secure);
     }
 
     /**
@@ -469,7 +463,7 @@ class Builder
     public function sortBy($sort_by, $sort_type = 'asc')
     {
         if (is_callable($sort_by)) {
-            $rslt = call_user_func($sort_by, $this->items->toArray());
+            $rslt = call_user_func($sort_by, $this->items->to[]);
 
             if (!is_array($rslt)) {
                 $rslt = array($rslt);
@@ -610,7 +604,7 @@ class Builder
      *
      * @return string
      */
-    public function render($type = 'ul', $parent = null, $children_attributes = array(), $item_attributes = array(), $item_after_calback = null, $item_after_calback_params = [])
+    public function render($type = 'ul', $parent = null, $children_attributes = [], $item_attributes = [], $item_after_calback = null, $item_after_calback_params = [])
     {
         $items = '';
 
@@ -665,7 +659,7 @@ class Builder
      *
      * @return string
      */
-    public function asUl($attributes = array(), $children_attributes = array(), $item_attributes = array(), $item_after_calback = null, $item_after_calback_params = [])
+    public function asUl($attributes = [], $children_attributes = [], $item_attributes = [], $item_after_calback = null, $item_after_calback_params = [])
     {
         return '<ul'.self::attributes($attributes).'>'.$this->render('ul', null, $children_attributes, $item_attributes, $item_after_calback, $item_after_calback_params).'</ul>';
     }
@@ -681,7 +675,7 @@ class Builder
      *
      * @return string
      */
-    public function asOl($attributes = array(), $children_attributes = array(), $item_attributes = array(), $item_after_calback = null, $item_after_calback_params = [])
+    public function asOl($attributes = [], $children_attributes = [], $item_attributes = [], $item_after_calback = null, $item_after_calback_params = [])
     {
         return '<ol'.self::attributes($attributes).'>'.$this->render('ol', null, $children_attributes, $item_attributes, $item_after_calback, $item_after_calback_params).'</ol>';
     }
@@ -697,7 +691,7 @@ class Builder
      *
      * @return string
      */
-    public function asDiv($attributes = array(), $children_attributes = array(), $item_attributes = array(), $item_after_calback = null, $item_after_calback_params = [])
+    public function asDiv($attributes = [], $children_attributes = [], $item_attributes = [], $item_after_calback = null, $item_after_calback_params = [])
     {
         return '<div'.self::attributes($attributes).'>'.$this->render('div', null, $children_attributes, $item_attributes, $item_after_calback, $item_after_calback_params).'</div>';
     }
@@ -711,7 +705,7 @@ class Builder
      */
     public static function attributes($attributes)
     {
-        $html = array();
+        $html = [];
 
         foreach ((array) $attributes as $key => $value) {
             $element = self::attributeElement($key, $value);
@@ -763,7 +757,7 @@ class Builder
      *
      * @return string
      */
-    public static function mergeStatic($new = null, array $old = array())
+    public static function mergeStatic($new = null, array $old = [])
     {
         // Parses the string into an associative array
         parse_str(preg_replace('/\s*([\w-]+)\s*=\s*"([^"]+)"/', '$1=$2&', $new), $attrs);
@@ -855,7 +849,6 @@ class Builder
             return $this->$prop;
         }
 
-        return $this->whereNickname($prop)
-                    ->first();
+        return $this->whereNickname($prop)->first();
     }
 }

src/Lavary/Menu/Collection.php
@@ -11,7 +11,7 @@ class Collection extends BaseCollection
      *
      * @param  mixed
      *
-     * @return Lavary\Menu\Collection
+     * @return Collection
      */
     public function attr()
     {
@@ -33,7 +33,7 @@ class Collection extends BaseCollection
      *
      * @param  mixed
      *
-     * @return Lavary\Menu\Collection
+     * @return Collection
      */
     public function data()
     {
@@ -55,7 +55,7 @@ class Collection extends BaseCollection
      *
      * @param  string
      *
-     * @return Lavary\Menu\Collection
+     * @return Collection
      */
     public function append($html)
     {
@@ -72,7 +72,7 @@ class Collection extends BaseCollection
      * @param mixed $html
      * @param mixed $key
      *
-     * @return Lavary\Menu\Collection
+     * @return Collection
      */
     public function prepend($html, $key = null)
     {

src/Lavary/Menu/Item.php
@@ -9,7 +9,7 @@ class Item
     /**
      * Reference to the menu builder.
      *
-     * @var Lavary\Menu\Menu
+     * @var Builder
      */
     protected $builder;
 
@@ -91,13 +91,13 @@ class Item
     public $isActive = false;
 
     /**
-     * Creates a new Lavary\Menu\MenuItem instance.
+     * Creates a new Item instance.
      *
-     * @param string            $title
-     * @param string            $url
-     * @param array             $attributes
-     * @param int               $parent
-     * @param \Lavary\Menu\Menu $builder
+     * @param string  $title
+     * @param string  $url
+     * @param array   $attributes
+     * @param int     $parent
+     * @param Builder $builder
      */
     public function __construct($builder, $id, $title, $options)
     {
@@ -152,7 +152,7 @@ class Item
     /**
      * Add a plain text item.
      *
-     * @return Lavary\Menu\Item
+     * @return Item
      */
     public function raw($title, array $options = array())
     {
@@ -165,6 +165,8 @@ class Item
      * Insert a seprator after the item.
      *
      * @param array $attributes
+     *
+     * @return Item
      */
     public function divide($attributes = array())
     {
@@ -191,7 +193,7 @@ class Item
      *
      * @param  mixed
      *
-     * @return string|Lavary\Menu\Item
+     * @return string|Item|array
      */
     public function attr()
     {
@@ -235,7 +237,7 @@ class Item
     /**
      * Prepends text or html to the item.
      *
-     * @return Lavary\Menu\Item
+     * @return Item
      */
     public function prepend($html)
     {
@@ -247,7 +249,7 @@ class Item
     /**
      * Appends text or html to the item.
      *
-     * @return Lavary\Menu\Item
+     * @return Item
      */
     public function append($html)
     {
@@ -259,7 +261,7 @@ class Item
     /**
      * Before text or html to the item.
      *
-     * @return Lavary\Menu\Item
+     * @return Item
      */
     public function before($html)
     {
@@ -271,7 +273,7 @@ class Item
     /**
      * After text or html to the item.
      *
-     * @return Lavary\Menu\Item
+     * @return Item
      */
     public function after($html)
     {
@@ -293,7 +295,7 @@ class Item
     /**
      * Returns children of the item.
      *
-     * @return Lavary\Menu\Collection
+     * @return Collection
      */
     public function children()
     {
@@ -313,7 +315,7 @@ class Item
     /**
      * Returns the parent item.
      *
-     * @return Lavary\Menu\Item
+     * @return Item
      */
     public function parent()
     {
@@ -323,7 +325,7 @@ class Item
     /**
      * Returns all childeren of the item.
      *
-     * @return Lavary\Menu\Collection
+     * @return Collection
      */
     public function all()
     {
@@ -360,7 +362,7 @@ class Item
      *
      * @param string $nickname
      *
-     * @return /Lavary/Menu/Item
+     * @return Item
      */
     public function nickname($nickname = null)
     {
@@ -378,7 +380,7 @@ class Item
      *
      * @param mixed $id
      *
-     * @return /Lavary/Menu/Item
+     * @return Item|int
      */
     public function id($id = null)
     {
@@ -394,9 +396,9 @@ class Item
     /**
      * Activate the item.
      *
-     * @param \Lavary\Menu\Item $item
+     * @param Item $item
      */
-    public function activate(\Lavary\Menu\Item $item = null, $recursion = false)
+    public function activate(Item $item = null, $recursion = false)
     {
         $item = is_null($item) ? $this : $item;
 
@@ -423,7 +425,7 @@ class Item
     /**
      * Make the item active.
      *
-     * @return Lavary\Menu\Item
+     * @return Item
      */
     public function active($pattern = null)
     {
@@ -447,7 +449,7 @@ class Item
      *
      * @param  mixed
      *
-     * @return string|Lavary\Menu\Item
+     * @return string|Item|array
      */
     public function data()
     {
@@ -482,6 +484,8 @@ class Item
      * Cascade data to children.
      *
      * @param array $args
+     *
+     * @return bool
      */
     public function cascade_data($args = array())
     {
@@ -494,6 +498,8 @@ class Item
         } else {
             $this->children()->data($args[0]);
         }
+
+        return true;
     }
 
     /**

src/Lavary/Menu/Link.php
@@ -7,7 +7,7 @@ class Link
     /**
      * Reference to the menu builder.
      *
-     * @var Lavary\Menu\Menu
+     * @var Builder
      */
     protected $builder;
 
@@ -42,7 +42,8 @@ class Link
     /**
      * Creates a hyper link instance.
      *
-     * @param array $path
+     * @param array   $path
+     * @param Builder $builder
      */
     public function __construct($path = array(), $builder)
     {
@@ -53,7 +54,7 @@ class Link
     /**
      * Make the anchor active.
      *
-     * @return Lavary\Menu\Link
+     * @return Link
      */
     public function active()
     {
@@ -66,7 +67,7 @@ class Link
     /**
      * Set Anchor's href property.
      *
-     * @return Lavary\Menu\Link
+     * @return Link
      */
     public function href($href)
     {
@@ -78,7 +79,7 @@ class Link
     /**
      * Make the url secure.
      *
-     * @return Lavary\Menu\Item
+     * @return Link
      */
     public function secure()
     {
@@ -87,12 +88,11 @@ class Link
         return $this;
     }
 
-    /**
+    /***
      * Add attributes to the link.
      *
-     * @param  mixed
-     *
-     * @return string|Lavary\Menu\Link
+     * @param mixed
+     * @return $this|array|mixed|null
      */
     public function attr()
     {
@@ -113,10 +113,11 @@ class Link
         return $this->attributes;
     }
 
-    /**
+    /***
      * Check for a method of the same name if the attribute doesn't exist.
      *
-     * @param  string
+     * @param $prop
+     * @return Link|string
      */
     public function __get($prop)
     {

src/Lavary/Menu/Menu.php
@@ -7,14 +7,14 @@ class Menu
     /**
      * Menu collection.
      *
-     * @var Illuminate\Support\Collection
+     * @var \Illuminate\Support\Collection
      */
     protected $collection;
 
     /**
      * List of menu items.
      *
-     * @var []\Lavary\Menu\Menu
+     * @var []Menu
      */
     protected $menu = [];
 
@@ -45,13 +45,15 @@ class Menu
      * @param string   $name
      * @param callable $callback
      *
-     * @return \Lavary\Menu\Menu
+     * @return Menu
      */
     public function makeOnce($name, $callback)
     {
-        if (!$this->exists($name)) {
-            return $this->make($name, $callback);
+        if ($this->exists($name)) {
+            return null;
         }
+
+        return $this->make($name, $callback);
     }
 
     /**
@@ -60,26 +62,28 @@ class Menu
      * @param string   $name
      * @param callable $callback
      *
-     * @return \Lavary\Menu\Menu
+     * @return Menu
      */
     public function make($name, $callback)
     {
-        if (is_callable($callback)) {
-            if (!array_key_exists($name, $this->menu)) {
-                $this->menu[$name] = new Builder($name, $this->loadConf($name));
-            }
-
-            // Registering the items
-            call_user_func($callback, $this->menu[$name]);
-
-            // Storing each menu instance in the collection
-            $this->collection->put($name, $this->menu[$name]);
-
-            // Make the instance available in all views
-            \View::share($name, $this->menu[$name]);
-
-            return $this->menu[$name];
+        if (!is_callable($callback)) {
+            return null;
         }
+
+        if (!array_key_exists($name, $this->menu)) {
+            $this->menu[$name] = new Builder($name, $this->loadConf($name));
+        }
+
+        // Registering the items
+        call_user_func($callback, $this->menu[$name]);
+
+        // Storing each menu instance in the collection
+        $this->collection->put($name, $this->menu[$name]);
+
+        // Make the instance available in all views
+        \View::share($name, $this->menu[$name]);
+
+        return $this->menu[$name];
     }
 
     /**
@@ -106,7 +110,7 @@ class Menu
      *
      * @param string $key
      *
-     * @return \Lavary\Menu\Item
+     * @return Item
      */
     public function get($key)
     {

src/Lavary/Menu/ServiceProvider.php
@@ -50,6 +50,6 @@ class ServiceProvider extends BaseServiceProvider
      */
     public function provides()
     {
-        return array(Menu::class);
+        return [Menu::class];
     }
 }

commit 1ee9472bca672a14512e6866e53edbaee5250dbd

Fix bug introduced by docblock cleanup
Docblock cleanup c9be1d2e724077576cde4c394c1483d121723457 introduced a bug when converting `toArray()` to `to[]`

src/Lavary/Menu/Builder.php
@@ -466,7 +466,7 @@ class Builder
     public function sortBy($sort_by, $sort_type = 'asc')
     {
         if (is_callable($sort_by)) {
-            $rslt = call_user_func($sort_by, $this->items->to[]);
+            $rslt = call_user_func($sort_by, $this->items->toArray());
 
             if (!is_array($rslt)) {
                 $rslt = array($rslt);