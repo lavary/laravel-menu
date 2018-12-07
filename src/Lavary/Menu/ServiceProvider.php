a301b61093aca4f7de67a966b6ddfd8cf2296695
Xiphoseer <Xiphoseer@users.noreply.github.com>
2018-12-07 18:24:41 +0100

    Use Blade::directive instead of Blade::extend (#237)
    * Add blade directives; Remove use of require_once
    
    * Update README for renamed lm_attrs
    
    * Use constants correctly
    
    * Update @lm_attrs syntax in examples
    
    * Add deprecation warning

--- src/Lavary/Menu/ServiceProvider.php
+++ src/Lavary/Menu/ServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Lavary\Menu;
 
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use Illuminate\Support\Facades\Blade;
 
 class ServiceProvider extends BaseServiceProvider
 {
@@ -26,13 +27,68 @@ class ServiceProvider extends BaseServiceProvider
         });
     }
 
+    // Patterns and Replace string for lm-attr
+    // Remove with next major version
+    const LM_ATTRS_PATTERN = '/(\s*)@lm-attrs\s*\((\$[^)]+)\)/';
+    const LM_ATTRS_REPLACE = '$1<?php $lm_attrs = $2->attr(); ob_start(); ?>';
+
+    // Patterns and Replace string for lm-endattr
+    // Remove with next major version
+    const LM_ENDATTRS_PATTERN = '/(?<!\w)(\s*)@lm-endattrs(\s*)/';
+    const LM_ENDATTRS_REPLACE = '$1<?php echo \Lavary\Menu\Builder::mergeStatic(ob_get_clean(), $lm_attrs); ?>$2';
+
+    /*
+     * Extending Blade engine. Remove with next major version
+     *
+     * @deprecated
+     * @return void
+     */
+    protected function bladeExtensions()
+    {
+        Blade::extend(function ($view, $compiler) {
+            if (preg_match(self::LM_ATTRS_PATTERN, $view)) {
+              \Log::debug("laravel-menu: @lm-attrs/@lm-endattrs is deprecated. Please switch to @lm_attrs and @lm_endattrs");
+            }
+            return preg_replace(self::LM_ATTRS_PATTERN, self::LM_ATTRS_REPLACE, $view);
+        });
+
+        Blade::extend(function ($view, $compiler) {
+            return preg_replace(self::LM_ENDATTRS_PATTERN, self::LM_ENDATTRS_REPLACE, $view);
+        });
+    }
+
+    /*
+     * Adding custom Blade directives.
+     */
+    protected function bladeDirectives()
+    {
+        /*
+         * Buffers the output if there's any.
+         * The output will be passed to mergeStatic()
+         * where it is merged with item's attributes
+         */
+        Blade::directive('lm_attrs', function ($expression) {
+            return '<?php $lm_attrs = ' . $expression . '->attr(); ob_start(); ?>';
+        });
+
+        /*
+         * Reads the buffer data using ob_get_clean()
+         * and passes it to MergeStatic().
+         * mergeStatic() takes the static string,
+         * converts it into a normal array and merges it with others.
+         */
+        Blade::directive('lm_endattrs', function ($expression) {
+            return '<?php echo \Lavary\Menu\Builder::mergeStatic(ob_get_clean(), $lm_attrs); ?>';
+        });
+    }
+
     /**
      * Bootstrap the application events.
      */
     public function boot()
     {
-        // Extending Blade engine
-        require_once 'blade/lm-attrs.php';
+        $this->bladeDirectives();
+        $this->bladeExtensions();
 
         $this->loadViewsFrom(__DIR__.'/resources/views', 'laravel-menu');
 
