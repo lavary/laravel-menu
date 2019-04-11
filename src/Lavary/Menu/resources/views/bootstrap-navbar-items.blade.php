lavary/laravel-menu

commit a301b61093aca4f7de67a966b6ddfd8cf2296695

Use Blade::directive instead of Blade::extend (#237)
* Add blade directives; Remove use of require_once

* Update README for renamed lm_attrs

* Use constants correctly

* Update @lm_attrs syntax in examples

* Add deprecation warning

src/Lavary/Menu/resources/views/bootstrap-navbar-items.blade.php
@@ -1,5 +1,5 @@
 @foreach($items as $item)
-  <li@lm-attrs($item) @if($item->hasChildren()) class="nav-item dropdown" @endif @lm-endattrs>
+  <li @lm_attrs($item) @if($item->hasChildren()) class="nav-item dropdown" @endif @lm_endattrs>
     @if($item->link) <a@lm-attrs($item->link) @if($item->hasChildren()) class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" @else class="nav-link" @endif @lm-endattrs href="{!! $item->url() !!}">
       {!! $item->title !!}
       @if($item->hasChildren()) <b class="caret"></b> @endif
@@ -9,9 +9,9 @@
     @endif
     @if($item->hasChildren())
       <ul class="dropdown-menu">
-        @include(config('laravel-menu.views.bootstrap-items'), 
+        @include(config('laravel-menu.views.bootstrap-items'),
 array('items' => $item->children()))
-      </ul> 
+      </ul>
     @endif
   </li>
   @if($item->divider)

src/Lavary/Menu/ServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Lavary\Menu;
 
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use Illuminate\Support\Facades\Blade;
 
 class ServiceProvider extends BaseServiceProvider
 {
@@ -26,13 +27,68 @@ class ServiceProvider extends BaseServiceProvider
         });
     }
 
+    // Patterns and Replace string for lm-attr
+    // Remove with next major version
+    const LM_ATTRS_PATTERN = '/(\s*)@lm-attrs\s*\((\$[^)]+)\)/';
+    const LM_ATTRS_REPLACE = '$1<?php $lm_attrs = $2->attr(); ob_start(); ?>';
+
+    // Patterns and Replace string for lm-endattr
+    // Remove with next major version
+    const LM_ENDATTRS_PATTERN = '/(?<!\w)(\s*)@lm-endattrs(\s*)/';
+    const LM_ENDATTRS_REPLACE = '$1<?php echo \Lavary\Menu\Builder::mergeStatic(ob_get_clean(), $lm_attrs); ?>$2';
+
+    /*
+     * Extending Blade engine. Remove with next major version
+     *
+     * @deprecated
+     * @return void
+     */
+    protected function bladeExtensions()
+    {
+        Blade::extend(function ($view, $compiler) {
+            if (preg_match(self::LM_ATTRS_PATTERN, $view)) {
+              \Log::debug("laravel-menu: @lm-attrs/@lm-endattrs is deprecated. Please switch to @lm_attrs and @lm_endattrs");
+            }
+            return preg_replace(self::LM_ATTRS_PATTERN, self::LM_ATTRS_REPLACE, $view);
+        });
+
+        Blade::extend(function ($view, $compiler) {
+            return preg_replace(self::LM_ENDATTRS_PATTERN, self::LM_ENDATTRS_REPLACE, $view);
+        });
+    }
+
+    /*
+     * Adding custom Blade directives.
+     */
+    protected function bladeDirectives()
+    {
+        /*
+         * Buffers the output if there's any.
+         * The output will be passed to mergeStatic()
+         * where it is merged with item's attributes
+         */
+        Blade::directive('lm_attrs', function ($expression) {
+            return '<?php $lm_attrs = ' . $expression . '->attr(); ob_start(); ?>';
+        });
+
+        /*
+         * Reads the buffer data using ob_get_clean()
+         * and passes it to MergeStatic().
+         * mergeStatic() takes the static string,
+         * converts it into a normal array and merges it with others.
+         */
+        Blade::directive('lm_endattrs', function ($expression) {
+            return '<?php echo \Lavary\Menu\Builder::mergeStatic(ob_get_clean(), $lm_attrs); ?>';
+        });
+    }
+
     /**
      * Bootstrap the application events.
      */
     public function boot()
     {
-        // Extending Blade engine
-        require_once 'blade/lm-attrs.php';
+        $this->bladeDirectives();
+        $this->bladeExtensions();
 
         $this->loadViewsFrom(__DIR__.'/resources/views', 'laravel-menu');
 

src/Lavary/Menu/blade/lm-attrs.php
@@ -1,56 +1,0 @@
-<?php
-
-use Illuminate\Support\Facades\Blade;
-
-/*
-|--------------------------------------------------------------------------
-| @lm-attrs
-|--------------------------------------------------------------------------
-|
-| Buffers the output if there's any.
-| The output will be passed to mergeStatic()
-| where it is merged with item's attributes
-|
-*/
-
-Blade::extend(function ($view, $compiler) {
-    $pattern = '/(\s*)@lm-attrs\s*\((\$[^)]+)\)/';
-
-    return preg_replace($pattern,
-                       '$1<?php $lm_attrs = $2->attr(); ob_start(); ?>',
-                        $view);
-});
-
-/*
-|--------------------------------------------------------------------------
-| @lm-endattrs
-|--------------------------------------------------------------------------
-|
-| Reads the buffer data using ob_get_clean()
-| and passes it to MergeStatic().
-| mergeStatic() takes the static string,
-| converts it into a normal array and merges it with others.
-|
-*/
-
-Blade::extend(function ($view, $compiler) {
-    $pattern = createPlainMatcher('lm-endattrs');
-
-    return preg_replace($pattern,
-                       '$1<?php echo \Lavary\Menu\Builder::mergeStatic(ob_get_clean(), $lm_attrs); ?>$2',
-                        $view);
-});
-
-if (!function_exists('createPlainMatcher')) {
-    /**
-     * Create a plain Blade matcher.
-     *
-     * @param string $function
-     *
-     * @return string
-     */
-    function createPlainMatcher($function)
-    {
-        return '/(?<!\w)(\s*)@'.$function.'(\s*)/';
-    }
-}

commit d7406165939362da831ad2b47ddac9cebdfc3af4

Add space before @lm_attrs
Related to issue #240 #241 
Caused by #237
Space was added before the @lm_attrs in the list item, but was missed in the anchor tag.

src/Lavary/Menu/resources/views/bootstrap-navbar-items.blade.php
@@ -1,6 +1,6 @@
 @foreach($items as $item)
   <li @lm_attrs($item) @if($item->hasChildren()) class="nav-item dropdown" @endif @lm_endattrs>
-    @if($item->link) <a@lm_attrs($item->link) @if($item->hasChildren()) class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" @else class="nav-link" @endif @lm_endattrs href="{!! $item->url() !!}">
+    @if($item->link) <a @lm_attrs($item->link) @if($item->hasChildren()) class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" @else class="nav-link" @endif @lm_endattrs href="{!! $item->url() !!}">
       {!! $item->title !!}
       @if($item->hasChildren()) <b class="caret"></b> @endif
     </a>